b7ec32044220de4bc03c62041bd7d376e6af3bc1
lksevans12 <lksevans12@gmail.com>
2016-08-12 18:14:53 -0400

    remove video

--- js/app.js
+++ js/app.js
@@ -4,66 +4,17 @@ $( document ).ready(function() {
 		$("#serivce-link").addClass("active")
 	})
 
-	$("#mute-button").on("click",function(event){
-		event.preventDefault();
-			var vid = document.getElementById("myVideo");
-			vid.muted = true;
-			$(this).toggle();
-			$("#volume-button").css('display', 'block');
-	});
-
-	$("#volume-button").on("click", function(event){
-		event.preventDefault();
-		var vid = document.getElementById("myVideo");
-			vid.muted = false;
-			$(this).toggle();
-			$("#mute-button").css('display', 'block');
-	});
-
-	var vid = document.getElementById("myVideo");
-	vid.onended = function(){
-		$(vid).hide();
-		$("#logoimg").css('display', 'block');
-		$("#logoimg").css('width', '100%');
-		$("#mute-button").hide();
-		$("#volume-button").hide();
-		$(".navbar").addClass('after-video');
-		$("#play-again-button").css('display', 'block')
-	}
-
-	$("#play-again-button").on("click", function(event){
-		event.preventDefault();
-		var vid = document.getElementById("myVideo");
-		$(".navbar").removeClass('after-video');
-		$("#logoimg").css('display', 'none');
-		$(vid).show();
-		$(vid).load();	
-		$(this).hide();
-		$("#mute-button").show();
-	});
-
-	$("#play-again-button").on("mouseover", function(){
-    $("#msg_div").css('display', 'inline-block')
-	}).mouseleave(function() {
-    $( "#msg_div" ).css('display', 'none')
-  });
-
-	var mainbottom = $('#services-content').offset().top + $('#services-content').height();
+	var mainbottom = $('.about-nav-anchor').offset().top + $('.about-nav-anchor').height();
  
 	$(window).on('scroll',function(){
-		var vid = document.getElementById("myVideo");
 
-    // we round here to reduce a little workload
     stop = Math.round($(window).scrollTop());
     if (stop > mainbottom) {
-    		$('.navbar').removeClass('after-video');
-        $('.navbar').addClass('past-main');
-    } else if (vid.paused) {
-        $('.navbar').removeClass('past-main');
-        $('.navbar').addClass('after-video');
+        $('.navbar').addClass('after-header');
+        $('.navbar-default').addClass('after-head')
    	} else {
-   		$('.navbar').removeClass('past-main')
-   		$('.navbar').removeClass('after-video')
+   		$('.navbar').removeClass('after-header')
+   		$('.navbar-default').removeClass('after-head')
    	}
 	});
 });
