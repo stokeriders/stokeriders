2f9beabb2fa3de8c747eec82bb6f44a797830d36
Laura Evans <lksevans12@gmail.com>
2017-11-08 00:25:44 -0600

    change logo on scroll

--- js/app.js
+++ js/app.js
@@ -12,9 +12,13 @@ $( document ).ready(function() {
     if (stop > mainbottom) {
         $('.navbar').addClass('after-header');
         $('.navbar-default').addClass('after-head')
+        $(".logo-large").css("display", "none");
+        $(".logo").css("display", "block");
    	} else {
    		$('.navbar').removeClass('after-header')
    		$('.navbar-default').removeClass('after-head')
+      $(".logo-large").css("display", "block");
+      $(".logo").css("display", "none");
    	}
 	});
 });
