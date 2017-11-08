56ada0b22aa3903810b9a7a3ad35cfd76df415d0
Laura Evans <lksevans12@gmail.com>
2017-11-08 00:06:07 -0600

    remove nav js

--- js/app.js
+++ js/app.js
@@ -18,9 +18,3 @@ $( document ).ready(function() {
    	}
 	});
 });
-
-$(document).ready(function () {
-    $("nav").find("li").on("click", "a", function () {
-        $('.navbar-collapse.in').collapse('hide');
-    });
-});
