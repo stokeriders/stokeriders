3215e50768a95e28cf5113217d3b9273d6ed248b
Laura Evans <lksevans12@gmail.com>
2017-11-25 13:57:19 -0600

    update pictures for tiffany

--- js/meet.js
+++ js/meet.js
@@ -1,16 +1,6 @@
 $( document ).ready(function() {
 
- $(".carousel").swipe({
-
-  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-
-    if (direction == 'left') $(this).carousel('next');
-    if (direction == 'right') $(this).carousel('prev');
-
-  },
-  allowPageScroll:"vertical"
-
-});
+ 
 
 
 });
